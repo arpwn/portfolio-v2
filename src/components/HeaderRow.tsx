d51636b711bc967afecf156437de0c8e89365445
Armandeux <armando@MacBook-Air-de-Jesus.local>
2026-05-30 12:41:34 -0600

    lastname updated

--- src/components/HeaderRow.tsx
+++ src/components/HeaderRow.tsx
@@ -8,7 +8,7 @@ export default function HeaderRow() {
         <AvatarImage src="https://ih1.redbubble.net/image.5758561875.8765/st,small,507x507-pad,600x600,f8f8f8.jpg" alt="@shadcn" />
         <AvatarFallback>CN</AvatarFallback>
       </Avatar>
-      <h1 className="text-2xl text-sky-400">Armando Gtz</h1>
+      <h1 className="text-2xl text-sky-400">Armando Nery</h1>
     </div>
   );
 }
